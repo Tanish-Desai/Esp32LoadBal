67d92c473315f3d7f7724ff713a407d18df15b7d
Tanish Somashekhar Desai <tanshikode79@gmail.com>
2026-02-02 02:38:38 +0530

    Added simple server-switching load balancer for 2 backend servers

--- esp32/MyFirstESP32/src/main.cpp
+++ esp32/MyFirstESP32/src/main.cpp
@@ -5,8 +5,11 @@ const char* ssid = "Superhungry";
 const char* password = "tea34320";
 
 // The "Real" Server (Your Laptop)
-const char* backend_ip = "10.176.160.78"; // CHANGE THIS to your laptop's IP
-const int backend_port = 8080;            // The port your laptop server listens on
+const char* backend_ip1 = "10.176.160.78"; // CHANGE THIS to your laptop's IP
+const int backend_port1 = 8080;            // The port your laptop server listens on
+
+const char* backend_ip2 = backend_ip1;
+const int backend_port2 = 8081;
 
 // The ESP32 Load Balancer Port
 const int listen_port = 80;
@@ -15,6 +18,7 @@ const int listen_port = 80;
 const int buffSize = 1024;
 uint8_t buffer[buffSize];
 
+bool server2 = false;
 WiFiServer publicServer(listen_port);
 
 void setup() {
@@ -40,10 +44,21 @@ void loop() {
     WiFiClient client = publicServer.available();
 
     if (client) {
-        Serial.println("New Client Connected to ESP32.");
+
+        // potato load balancer (simply switches servers for each client)
+        server2 = !server2; 
+        Serial.print("Server switched to ");
+        Serial.println(server2?8081:8080);
+
+        Serial.println("\nNew Client Connected to ESP32.");
 
         // Attempt to connect to the Backend Server (Laptop)
         WiFiClient backend;
+
+        // Load Balancing: (const as compiler doesn't allow otherwise)
+        const char* backend_ip = server2 ? backend_ip2 : backend_ip1;
+        const int backend_port = server2 ? backend_port2 : backend_port1;
+
         if (backend.connect(backend_ip, backend_port)) {
             Serial.println("Connected to Backend Server. Bridging traffic...");
 
@@ -85,9 +100,13 @@ void loop() {
                 }
             }
             backend.stop();
-            Serial.println("Session Closed.");
+            Serial.println("Session Closed.\n");
         } else {
             Serial.println("Failed to connect to Backend Server.");
+            Serial.print("Backend IP:Port - ");
+            Serial.print(backend_ip);
+            Serial.print(":");
+            Serial.println(backend_port);
             client.println("HTTP/1.1 502 Bad Gateway");
             client.println("Connection: close");
             client.println();
